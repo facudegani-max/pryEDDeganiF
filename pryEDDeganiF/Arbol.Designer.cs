f0fe9f42abc088bbbc3f79a79bafee840e44e629
facudegani-max <facudegani@gmail.com>
2026-05-21 12:01:09 -0300

    dfg

---
+++ pryEDDeganiF/Arbol.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace pryEDDeganiF
+{
+    partial class Arbol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Arbol));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvListaDoble = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tramite = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNombre_Agregar = new System.Windows.Forms.TextBox();
+            this.txtTramite_Agregar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo_Agregar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.cmbCodigoEliminar = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEquilibrar = new System.Windows.Forms.Button();
+            this.optPre_Order = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaDoble)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 44;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.optPre_Order);
+            this.groupBox2.Controls.Add(this.dgvListaDoble);
+            this.groupBox2.Location = new System.Drawing.Point(12, 172);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(576, 151);
+            this.groupBox2.TabIndex = 43;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listado en Grilla y Lista";
+            // 
+            // dgvListaDoble
+            // 
+            this.dgvListaDoble.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaDoble.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Nombre,
+            this.Tramite});
+            this.dgvListaDoble.Location = new System.Drawing.Point(220, 19);
+            this.dgvListaDoble.Name = "dgvListaDoble";
+            this.dgvListaDoble.Size = new System.Drawing.Size(350, 121);
+            this.dgvListaDoble.TabIndex = 15;
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Tramite
+            // 
+            this.Tramite.HeaderText = "Tramite";
+            this.Tramite.Name = "Tramite";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNombre_Agregar);
+            this.groupBox1.Controls.Add(this.txtTramite_Agregar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtCodigo_Agregar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(249, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(160, 105);
+            this.groupBox1.TabIndex = 42;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Elemento Agregado";
+            // 
+            // txtNombre_Agregar
+            // 
+            this.txtNombre_Agregar.Location = new System.Drawing.Point(76, 46);
+            this.txtNombre_Agregar.Name = "txtNombre_Agregar";
+            this.txtNombre_Agregar.Size = new System.Drawing.Size(75, 20);
+            this.txtNombre_Agregar.TabIndex = 8;
+            // 
+            // txtTramite_Agregar
+            // 
+            this.txtTramite_Agregar.Location = new System.Drawing.Point(76, 77);
+            this.txtTramite_Agregar.Name = "txtTramite_Agregar";
+            this.txtTramite_Agregar.Size = new System.Drawing.Size(75, 20);
+            this.txtTramite_Agregar.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // txtCodigo_Agregar
+            // 
+            this.txtCodigo_Agregar.Location = new System.Drawing.Point(76, 15);
+            this.txtCodigo_Agregar.Name = "txtCodigo_Agregar";
+            this.txtCodigo_Agregar.Size = new System.Drawing.Size(75, 20);
+            this.txtCodigo_Agregar.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tramite";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.cmbCodigoEliminar);
+            this.groupBox.Controls.Add(this.label4);
+            this.groupBox.Controls.Add(this.btnEliminar);
+            this.groupBox.Location = new System.Drawing.Point(415, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(167, 105);
+            this.groupBox.TabIndex = 41;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Elemento Eliminado";
+            // 
+            // cmbCodigoEliminar
+            // 
+            this.cmbCodigoEliminar.FormattingEnabled = true;
+            this.cmbCodigoEliminar.Location = new System.Drawing.Point(52, 18);
+            this.cmbCodigoEliminar.Name = "cmbCodigoEliminar";
+            this.cmbCodigoEliminar.Size = new System.Drawing.Size(99, 21);
+            this.cmbCodigoEliminar.TabIndex = 33;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Codigo";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.Location = new System.Drawing.Point(15, 45);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(136, 43);
+            this.btnEliminar.TabIndex = 34;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnAgregar.Location = new System.Drawing.Point(269, 123);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(140, 43);
+            this.btnAgregar.TabIndex = 40;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // btnEquilibrar
+            // 
+            this.btnEquilibrar.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnEquilibrar.Location = new System.Drawing.Point(426, 123);
+            this.btnEquilibrar.Name = "btnEquilibrar";
+            this.btnEquilibrar.Size = new System.Drawing.Size(140, 43);
+            this.btnEquilibrar.TabIndex = 45;
+            this.btnEquilibrar.Text = "Equilibrar";
+            this.btnEquilibrar.UseVisualStyleBackColor = false;
+            // 
+            // optPre_Order
+            // 
+            this.optPre_Order.AutoSize = true;
+            this.optPre_Order.Location = new System.Drawing.Point(26, 36);
+            this.optPre_Order.Name = "optPre_Order";
+            this.optPre_Order.Size = new System.Drawing.Size(73, 17);
+            this.optPre_Order.TabIndex = 16;
+            this.optPre_Order.TabStop = true;
+            this.optPre_Order.Text = "Pre-Orden";
+            this.optPre_Order.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(26, 106);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(82, 17);
+            this.radioButton2.TabIndex = 18;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ascendente";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(26, 70);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.TabIndex = 19;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Pre-Orden";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Arbol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 334);
+            this.Controls.Add(this.btnEquilibrar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Arbol";
+            this.Text = "Arbol";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaDoble)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvListaDoble;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tramite;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNombre_Agregar;
+        private System.Windows.Forms.TextBox txtTramite_Agregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigo_Agregar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.ComboBox cmbCodigoEliminar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEquilibrar;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton optPre_Order;
+    }
+}
